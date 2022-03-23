38db0ed7b214710f5bd60de58ba6e93350e8250a
vinay.john <vinay.john@powerschool.com>
2022-03-23 20:45:53 +0530

    Adding Unit test for LocationsListByDistanceConverter.

--- app/src/test/java/com/nordsec/locationapp/locations/domain/LocationsListByDistanceConverterTest.kt
+++ app/src/test/java/com/nordsec/locationapp/locations/domain/LocationsListByDistanceConverterTest.kt
@@ -1,24 +1,23 @@
 package com.nordsec.locationapp.locations.domain
 
-
 import com.google.common.truth.Truth
+import com.nordsec.locationapp.common.LocationWithDistance
 import com.nordsec.locationapp.locations.data.Location
-import com.nordsec.locationapp.locations.data.Locations
 import org.junit.Before
 import org.junit.Test
 
-class LocationsListConverterTest {
+class LocationsListByDistanceConverterTest {
 
-    private lateinit var locationsListConverter: LocationsListConverter
+    private lateinit var locationsListByDistanceConverter:  LocationsListByDistanceConverter
 
     @Before
     fun setUp() {
-        locationsListConverter = LocationsListConverter()
+        locationsListByDistanceConverter = LocationsListByDistanceConverter()
     }
 
     @Test
     fun `should convert list of location api response to locations viewstate`() {
-        val convertedViewState = locationsListConverter.apply(getMockLocationListResponse())
+        val convertedViewState = locationsListByDistanceConverter.apply(getMockLocationListResponse())
         val expected = LocationsViewState.Success(getSortedLocationDomainModels())
         Truth.assertThat(convertedViewState).isEqualTo(expected)
     }
@@ -27,33 +26,40 @@ class LocationsListConverterTest {
      * Returns the mock location List response
      */
 
-    private fun getMockLocationListResponse(): Locations {
-        return Locations(
-            location = listOf(
-                Location(
+    private fun getMockLocationListResponse(): List<LocationWithDistance> {
+        return listOf(
+
+            LocationWithDistance(
+                location = Location(
                     city = "New York",
                     latitude = 40.712784F,
                     longitude = -74.00594F
-                ),
-                Location(
+                ),0.0
+            ),
+            LocationWithDistance(
+                location = Location(
                     city = "Los Angeles",
                     latitude = 34.052235F,
                     longitude = -118.24368F
-                ),
-                Location(
+                ),0.0
+            ),
+            LocationWithDistance(
+                location = Location(
                     city = "Chicago",
                     latitude = 41.878113F,
                     longitude = -87.6298F
-                )
+                ),0.0
             )
+
+
         )
     }
 
     private fun getSortedLocationDomainModels(): List<LocationDomainModel> {
         return listOf(
-            LocationDomainModel("Chicago"),
+            LocationDomainModel("New York"),
             LocationDomainModel("Los Angeles"),
-            LocationDomainModel("New York")
+            LocationDomainModel("Chicago")
         )
     }
 
