432e44a4f4cca5f825045d4c94807bacbefb4842
vinay.john <vinay.john@powerschool.com>
2022-03-23 21:42:55 +0530

    Code Cleanup

--- app/src/main/java/com/nordsec/locationapp/locations/ui/activities/MainActivity.kt
+++ app/src/main/java/com/nordsec/locationapp/locations/ui/activities/MainActivity.kt
@@ -31,8 +31,6 @@ class MainActivity : AppCompatActivity() {
         setContentView(binding.root)
 
         initialise()
-
-        Utils().getLocationListSortedWithSelectedLocation("New York")
     }
 
     private fun initialise(){
